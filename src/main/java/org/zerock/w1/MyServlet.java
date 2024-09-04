5d2323cd6fb741ca536a86000bbbb21feb745fe3
Park, minsu <cmwmss59@gmail.com>
2024-09-04 18:42:06 +0900

    Servlet 경로 설정과 이름 임의로 지정해보기 !

--- src/main/java/org/zerock/w1/MyServlet.java
+++ src/main/java/org/zerock/w1/MyServlet.java
@@ -5,13 +5,23 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 @WebServlet(name="myServlet",urlPatterns = "/my")
+//name은 Servlet의 이름을 직접 지정하는 부분임 !
 public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response)
-    {
+                         HttpServletResponse response) throws IOException {
         response.setContentType("text/html");//
+        //응답의 ContentType을 text/html로 두는거임
+
+        PrintWriter out=response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>MyServlethello</h1>");
+        out.println("</body></html>");
+
     }
 }
